6dcb2e78ede2a47ee92d1d20537093bbb59838da
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-07-29 08:57:29 +0530

    Update duplicate_array.c

--- duplicate_array.c
+++ duplicate_array.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
 int main(){
     
-    int num, count;
+    int num;
     
     printf("how many values do you want to store: ");
     scanf("%d", &num);
     int a[num];
+    int co[num], k=0;
     for(int i=0;i<num;i++){
         scanf("%d", &a[i]);   
     }
-   
-    
-
     
     for(int i=0; i<num-1; i++){
         for(int j=i+1;j<num;j++){
@@ -22,18 +20,34 @@ int main(){
             }
         }
     }
+    int du[num];
     printf("After array sort:");
     for(int i=0;i<num;i++){
-        
-            printf(" %d", a[i]);
+            du[i]=a[i];
+            printf(" %d", du[i]);
     }
+    
+
     for(int i=0;i<num-1;i++){
         for(int j=i+1;j<num;j++){
-            if(a[i]==a[j]){
-                count++;
-            }
+            if(co[k]!=du[i]){
+                if(du[i] == du[j]){
+                   co[i] = du[i];
+                   
+                }
+            }  
+            
         }
+     k++;
+   
     }
-    printf("\nArray duplicate count: %d", count);
+    
+    printf("\nArray duplicate print: ");
+
+    for(int i=0;i<k;i++){
+    printf(" %d", co[i]);
+    }
+    
     return 0;
+
 }
