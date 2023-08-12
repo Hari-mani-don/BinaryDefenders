101dbaaa3608402f3b18db6ad00698b453bcda5c
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-08-12 19:04:08 +0530

    Create rhombusStarPatterns3.c

---
+++ rhombusStarPatterns3.c
@@ -0,0 +1,24 @@
+/* Rhombus Star patterns*/
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Input number of Rows: ");
+    scanf("%d", &n);
+    int temp=n-1;
+    int x=n*2;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<x;j++){
+            if(j>=1 && j<=temp){
+                printf(" ");
+            }
+            else{
+                printf("*");
+            }
+        }
+        temp--;
+        x--;
+        printf("\n");
+    }
+    return 0;
+}
