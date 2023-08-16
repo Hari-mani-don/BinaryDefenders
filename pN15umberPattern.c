30eef679031d8168161b757887b6e893da83f979
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-08-16 19:35:12 +0530

    Create pN15umberPattern.c

---
+++ pN15umberPattern.c
@@ -0,0 +1,20 @@
+//Number Pattern 15
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Input number of Rows: ");
+    scanf("%d", &n);
+    for(int row=1;row<2*n;row++){
+        for(int col=1;col<2*n;col++){
+            if(row==col || col==(2*n)-row){
+                printf(" ");
+            }
+            else{
+                printf("1");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
