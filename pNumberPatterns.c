d9fa78ae36f464c9ff447ebf13fc268a152253e0
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-08-16 15:06:38 +0530

    Create pNumberPatterns.c

---
+++ pNumberPatterns.c
@@ -0,0 +1,15 @@
+//pNumber Patterns
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Input number of Rows: ");
+    scanf("%d", &n);
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=n;col++){
+            printf("1");
+        }
+        printf("\n");
+    }
+    return 0;
+}
