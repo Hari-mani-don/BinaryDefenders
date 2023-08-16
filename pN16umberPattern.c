df70ee60425a85a5fb317a7630b9ac074443993b
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-08-16 19:36:31 +0530

    Create pN16umberPattern.c

---
+++ pN16umberPattern.c
@@ -0,0 +1,20 @@
+//Number Pattern 16
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Input number of Rows: ");
+    scanf("%d", &n);
+    for(int row=1;row<2*n;row++){
+        for(int col=1;col<2*n;col++){
+            if(row==col || col==(2*n)-row){
+                printf("X");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
