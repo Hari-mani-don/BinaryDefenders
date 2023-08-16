fb06e4a7ea50257dd5846ef103e6ea8c4479191f
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-08-16 15:31:27 +0530

    Create pN3mbersPatternV2.c

---
+++ pN3mbersPatternV2.c
@@ -0,0 +1,18 @@
+//Number of Patterns V3
+
+#include<stdio.h>
+int main(){
+    int row, col;
+    printf("Input number of Rows: ");
+    scanf("%d", &row);
+    printf("Input numbr of Coloums: ");
+    scanf("%d", &col);
+    for(int i=1;i<=row;i++){
+        for(int j=0;j<col;j++){
+            printf("%d",(j%2));    
+            
+        }
+        printf("\n");
+    }
+            return 0;
+}
