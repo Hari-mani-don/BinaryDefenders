c1013917219fae71780d7f6cbffb9db83957e2cf
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-02 20:46:21 +0530

    Create strRemoveLastFrequenceV1.c

---
+++ strRemoveLastFrequenceV1.c
@@ -0,0 +1,38 @@
+//Remove Last Frequrence in a string V1.c
+
+#include<stdio.h>
+#define SIZE 100
+char removeFirestFrequence(char *a, char search);
+int main(){
+    char b[SIZE];
+    char search;
+    printf("Enter string: ");
+    scanf("%[^\n]", b);
+    printf("Search input to remove character: ");
+    scanf(" %c", &search);
+    printf("Remove First Frequrence Of character: \n");
+    removeFirestFrequence(b, search);
+    return 0;
+}
+char removeFirestFrequence(char *a, char search){
+    int i=0, j=0,se=0, count=0;
+    while(a[i]!='\0'){
+        if(a[i]==search){
+          count++;
+        }
+        i++;
+    }
+    
+    while(a[j]!='\0'){
+        if(a[j]==search){
+            se++;
+        }
+        if(a[j]==search && se==count){
+           se++;
+        }
+        else{
+            printf("%c", a[j]);
+        }
+        j++;
+    }
+}
