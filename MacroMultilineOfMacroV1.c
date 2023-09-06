564fb52c7bed229f6ca825785b4eea6c10360d9c
Hari143mani <121375212+Hari-mani-don@users.noreply.github.com>
2023-09-06 16:08:47 +0530

    Create MacroMultilineOfMacroV1.c

---
+++ MacroMultilineOfMacroV1.c
@@ -0,0 +1,19 @@
+//Multiline of Macro swap tow number
+#include<stdio.h>
+#define SWAP(a, b) a=a*b; \
+b=a/b; \
+a=a/b;
+
+int main(){
+    int a=10;
+    int b=20;
+    
+    printf("Before swap a= %d\n",a);
+    printf("Befor swap b= %d\n",b);
+    
+    SWAP(a, b);
+    printf("\nAfter swap a= %d\n", a);
+    printf("After swap b= %d\n", b);
+    
+    return 0;
+}
